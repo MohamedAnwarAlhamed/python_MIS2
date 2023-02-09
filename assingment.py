4318bca40322a1bba86a758c676108cf06f47679
ahmedalduais <ahmedaldouois@gmail.com>
2023-02-09 04:11:00 +0300

    add the assignment

--- assingment.py
+++ assingment.py
@@ -1,105 +1,79 @@
 
-#احمد عبدالله الدعيس
+#ابراهيم محمد صلاح
 # 1
 a=[53,32,19,79,25,5,47]
-
-max = a[0]
-min = a[0]
-d = 0
+min_=0
+max_=0
 for i in a:
+ if i >max_:
+   max_=i
 
-    if i > max:
-        max = i
-    if i < min:
-        min = i
+   min_=max_
+for i in a:
+ if i <min_:
+   min_=i
 
-print("max = ", max,"min = ", min)
+print(f"the maximum number is {max_}" )
+print(f"the minimum number is {min_}")
+
 
 # 2
-a=int (input("enter number 1: "))
-b=int (input("enter number 2: "))
-
-c=a/b
-if a%b==0:
-    print(True)
-elif a%b==1:
-    print(False)
+a=int(input ("enter the first number"))
+s=int(input ("enter the second number"))
+if s%a==0 or a%s==0:
+  print(True)
+else:
+  print (False)
 # 3
-a=[]
-
-print(type (a))
+s=input ("enter the variable is what")
+print(type(s))
 # 4
 numbers = [386, 462, 47, 418, 907, 344, 236, 375, 823, 566, 597, 978, 328, 615, 953, 345,399, 162, 758, 219, 918, 237, 412, 566, 826, 248,866, 950, 626, 949, 687, 217,815, 67, 104, 58, 512, 24, 892, 894, 767, 553, 81, 379,843, 831, 445, 742, 717,958,743, 527]
+
 for i in numbers:
-    if i%2==0:
-        print(i)
-        if i ==237:
-            break
-
+ if i %2==0 :
+   print (i)
+ if i==237  :
+   break;
 # 5
-a=int (input("enter number:"))
-group=[1,5,8,3,3]
-if a in group:
-    print(True)
+numbers = [386, 462, 47, 418, 907, 344, 236, 375, 823, 566, 597, 978, 328, 615, 953, 345,399, 162, 758, 219, 918, 237, 412, 566, 826, 248,866, 950, 626, 949, 687, 217,815, 67, 104, 58, 512, 24, 892, 894, 767, 553, 81, 379,843, 831, 445, 742, 717,958,743, 527]
+a=int(input("enter the number for check if the number in the group of values. "))
+if a in numbers:
+ print (True)
 else:
-    print(False)
+ print (False)
 # 6
-a=[1,2,3,3,3,3,4,5]
-print(set(a))
+sf=[1,2,3,3,3,4,5,5]
+sd=[]
+i=len(sf)
+h=0
+while h<i:
+ if sf[h] not in sd:
+  sd.append(sf[h])
+ h+=1
+print(sd)
 # 7
-a=input("enter:")
-
-x=""
-z=len(a)
-for i in a:
-    x+=a[z-1]
-    z=z-1
-d=x
-print(d)
-if d==a:
-    print(True)
+str1=str(input ("enter string is paliendrome or not"))
+str2=len(str1)
+str2*=-1
+str3=str1[-1:str2-1:-1]
+if str3==str1:
+ print("yes is paliendrome")
 else:
-    print(False)
+ print("no is not paliendrome")
 # 8
-num= int (input("enter number"))
-md=1
-for i in range(1,num+1):
-    md=md*i
-    print(md)
+from math import *
+g=int(input("ener the radius of the circle"))
+print(factorial(g))
 # 9
-class rectangle:
-    def __init__(self,length,width):
-        self.length=length
-        self.width=width
-    def area_rectangle (self,length,width):
-        area=length*width
-        return area
-    def mference_rectangle (self,length,width):
-        mference1=2*(length+width)
-        return mference1
-
-x=int (input("enter length"))
-y=int (input("enter width"))
-rectangle1=rectangle(x,y)
-print(rectangle1.area_rectangle(x,y))
-print(rectangle1.mference_rectangle(x,y))
+i=float(input("ادخل طول المستطيل"))
+f=float(input ("ادخل عرض المستطيل"))
+print(i*f)
 # 10
-p = 3.1415
-class Circle:
-
-    def __init__(self,p,radius):
-        self.p=p
-        self.radius=radius
-    def circle_area(self,p,radius):
-        area=p*radius*radius
-        return area
-    def circle_mference (self,p,radius):
-        mference=2*p*radius
-        return mference
-r=float(input("enter radius"))
-circle1=Circle(p,r)
-print(circle1.circle_area(p,r))
-print(circle1.circle_mference(p,r))
+i=float(input("ener the radius of the circle"))
+print("the perimeter of the circle is",2*3.14*i)
+print("the area of the circle is",3.14*i**2)
 
 
-#احمد عبدالله الدعيس
+
+#ابراهيم محمد صلاح
